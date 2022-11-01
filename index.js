3c41013829c9acb3d37abe052882a234965222a9
Ryan See <ryanjosephsee@gmail.com>
2022-11-01 08:49:26 -0700

    commit tree 63a8408f5809c561a6952054fce2a9efaab93120

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
+}
+function appendCat(name) {
+   const moreCats = [...cats, name]
+   return moreCats;
+}
+
+function prependCat(name) {
+    const evenMoreCats = [name, ...cats]
+    return evenMoreCats;
+}
+function removeLastCat() {
+    const lessCats = cats.slice(0,-1)
+    return lessCats
+}
+function removeFirstCat() {
+    const lessCat = cats.slice(1)
+    return lessCat
+}
